9d1df5883faec9180d4ef0b07ef57bf5873c6943
Martin Hanacek <246822@vutbr.cz>
2026-04-08 10:50:21 +0200

    3

--- searching.py
+++ searching.py
@@ -36,39 +36,63 @@ def linear_search(numbers, searched_number):
     }
 
 
-def binary_search():
+def binary_search(numbers, searched_number):
+    middle = len(numbers) // 2
+    left_margin = numbers[0]
+    right_margin = numbers[-1]
+    while len(numbers) != 0:
+        if middle == searched_number:
+            return middle
+        elif middle < searched_number:
+            numbers = numbers[middle:right_margin]
+        elif middle > searched_number:
+            numbers = numbers[left_margin:middle]
+    return None
 
 
+import time
 
+numbers = [4, 8, 15, 16, 23, 42, 55, 78, 91, 120]
+target = 78
 
-#  #def pattern_search(sequence, pattern):
-# sequence = 'ATGACGGAATATAAGCTAGGTGGTGGCTGGGCAGTCCGCGCTGATAGGGCAAGAGTGCGCGTACCATACCACGCTAAGCCATATAGGGCATCAGTCAGCCTGGCA'
-# pattern = 'AT'
-# n = len(sequence)
-# m = len(pattern)
-# count = 0
-# position = set()
-# for index in range (n - (m - 1)):
-#     is_same = True
-#
-#     if sequence[index: index + n] == pattern:
-#         count += 1
-#     for index_pattern in range(m):
-#         print(sequence[index + index_pattern])
-#         if sequence[index + index_pattern] != pattern[index_pattern]:
-#             is_same = False
-#     if is_same:
-#         position.add(index)
-#     print(position)
-#     print(count)
-#     print(is_same)
+start = time.perf_counter()
+
+for number in numbers:
+    if number == target:
+        break
+
+end = time.perf_counter()
+
+duration = end - start
+print(f"Měření trvalo {duration:.8f} s")
+
+def pattern_search(sequence, pattern):
+    n = len(sequence)
+    m = len(pattern)
+    count = 0
+    position = []
+    for i in range (n - (m - 1)):
+        is_same = True
+
+        if sequence[i: i + n] == pattern:
+            count += 1
+        for index_pattern in range(m):
+            if sequence[i + index_pattern] != pattern[index_pattern]:
+                is_same = False
+        if is_same:
+            position.append(i)
+
 
 
 
 
 def main():
-    pass
-
+    sequential = read_data("sequential.json", "unordered_numbers")
+    print(sequential)
+    linear = linear_search("sequential.json", "unordered_numbers")
+    print(linear)
+    binary = binary_search("sequential.json", "unordered_numbers")
+    print(binary)
 
 if __name__ == '__main__':
     main()
